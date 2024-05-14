42f32116dd97e1e542386de9b28d160e7677e7ca
BachoKartsivadze <bachoqarcivadze62@gmail.com>
2024-05-14 00:35:20 -0700

    Add project files.

---
+++ ContactsMenegerConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using ContactsMenegerConsoleApp;
+using System;
+using System.Collections.Generic;
+
+
+
+ManagerModel managerModel = new ManagerModel();
+while (true)
+{
+    Console.WriteLine("Simple Contacts Manager");
+    Console.WriteLine("1. Add Contact");
+    Console.WriteLine("2. View List of Contacts");
+    Console.WriteLine("3. Remove Contact by email");
+    Console.WriteLine("Press Ctrl+M to go back to the main screen.");
+
+    ConsoleKeyInfo keyNumber = Console.ReadKey();
+    Console.WriteLine();
+
+    if (keyNumber.Modifiers == ConsoleModifiers.Control && keyNumber.Key == ConsoleKey.M)
+    {
+        Console.Clear();
+        continue;
+    }
+
+    switch (keyNumber.KeyChar)
+    {
+        case '1':
+            managerModel.AddContact();
+            break;
+        case '2':
+            managerModel.ViewContacts();
+            break;
+        case '3':
+            managerModel.RemoveContact();
+            break;
+        default:
+            Console.WriteLine("Invalid option. Please try again.");
+            break;
+    }
+}
+
---
+++ ContactsMenegerConsoleApp/managerModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsMenegerConsoleApp
+{
+    public class ManagerModel
+    {
+        static List<CMContact> contacts = new List<CMContact>();
+
+        public void AddContact()
+        {
+            Console.WriteLine("Enter Name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter Phone:");
+            string phone = Console.ReadLine();
+
+            Console.WriteLine("Enter eMail:");
+            string email = Console.ReadLine();
+
+            CMContact newContact = new CMContact { Name = name, PhoneNumber = phone, Email = email };
+            contacts.Add(newContact);
+
+            Console.WriteLine("Contact added successfully.");
+        }
+
+        public void ViewContacts()
+        {
+            Console.WriteLine("List of Contacts:");
+            if (contacts.Count == 0)
+            {
+                Console.WriteLine("No contacts found.");
+                return;
+            }
+
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"Name: {contact.Name}");
+                Console.WriteLine($"Phone: {contact.PhoneNumber}");
+                Console.WriteLine($"Email: {contact.Email}");
+                Console.WriteLine($"..............");
+                Console.WriteLine();
+            }
+        }
+
+        public void RemoveContact()
+        {
+            Console.WriteLine("Enter the email of the contact to remove:");
+            string emailToRemove = Console.ReadLine();
+
+            CMContact contactToRemove = contacts.Find(c => c.Email == emailToRemove);
+            if (contactToRemove == null)
+            {
+                Console.WriteLine("No contact found with that email.");
+                return;
+            }
+
+            contacts.Remove(contactToRemove);
+            Console.WriteLine("Contact removed successfully.");
+        }
+    }
+}
